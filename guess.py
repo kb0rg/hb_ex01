555004696bae9fbcf327b13abad935a2067472c6
Hackbright Student <info@hackbrightacademy.com>
2015-01-13 17:17:59 -0800

    added best score counter and print message oon exit.

--- guess.py
+++ guess.py
@@ -1,26 +1,46 @@
 import random
 
 def gameControl():
+    
     num = random.randint(0, 4)
+    
     print "Howdy, what's your name?"
     name = raw_input('')
     print name + ", I'm thinking of a number between 1 and 100. Try to guess my number."
+
     evaluate(num, name)
 
+
+
+
 def evaluate(num, name):
+
     playAgain = True
+
     while playAgain == True:
+        
         guess = -1
         count = 0
+        bestScore = 100000
+
         while(guess != num)     :
             tuple = validate(guess, num, count)
             guess = tuple[0]
-            count = tuple[1]
+            count = tuple[1]     
+
         print 'Well done, ' + name + '! You found my number in ' + str(count) + ' tries!'
+        if count < bestScore:
+            bestScore = count
+        print "Your current best score is: " + str(bestScore)
         print "Wanna play again?"
+        
         pa = raw_input('...?')
         if pa.upper() not in "YES Y":
             playAgain = False
+            print "bye!"
+
+
+
 
 def validate(guess, num, count):
     try: 
@@ -33,11 +53,15 @@ def validate(guess, num, count):
         elif guess < 0 or guess > 100:
             print "That number is way out of range of 1 to 100!" 
         count += 1 
-        print str(count) + 'count validate'
-        print str(guess) + 'guess validate'
         return (guess, count)
     except ValueError:
         print "Please enter a valid integer"
 
 
+
+
 gameControl()
+
+
+
+
